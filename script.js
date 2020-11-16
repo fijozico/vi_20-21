a120f20a1cc5ad023abdbe768987a0cc49deef53
fijozico <fijozico@hotmail.com>
2020-11-16 13:08:30 +0000

    Slight modifications

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var map;
 var no_countries_mode = 1;
-var country_colours = {"hover": "#72de78", "normal": "#aaa"}
+var country_colours = {"hover": "#72de78", "active": " #4bc551", "normal": "#aaa"}
+
+// holds the current active countries
+var active_countries = ["",""];
 
 function resizeMap() {
     var map_container = map.parent();
@@ -73,13 +76,14 @@ function bindCountryHover() {
         // this way, it deals with all the country's paths, along with the list item
         $("svg#map-svg path[data-country=" + this.dataset.country + "], ul#country-pick li#li-" + this.dataset.country).each(function () {
             // if it's the path, just changes its color
-            if (this.localName == "path")
+            if (this.localName == "path" && $(this).attr("fill") != country_colours.active)
                 $(this).attr("fill", country_colours.hover);
 
             // if it's the list item, only "hovers" it if item not active
             // important because list item may be "hovered" (temporary highlight) or "active" (permanent highlight)
-            else if (!this.classList.contains("active"))
-                $(this).addClass("hovered");
+            else if (!this.dataset.active || this.dataset.active === "false") {
+                this.dataset.hovered = true;
+            }
         });
     });
 }
@@ -87,15 +91,14 @@ function bindCountryHover() {
 function bindCountryUnhover() {
     // taking the mouse off the country (map or list)
     $("svg#map-svg path:not(.greyed-out), ul#country-pick li").on("mouseout", function () {
-        var class_ = $(this).attr("class");
-        if (class_ != undefined && class_.includes("active"));
+        if (this.dataset.active === "true");
 
         else $("svg#map-svg path[data-country=" + this.dataset.country + "], ul#country-pick li#li-" + this.dataset.country).each(function () {
             if (this.localName == "path")
                 $(this).attr("fill", country_colours.normal);
 
             else
-                $(this).removeClass("hovered");
+                this.dataset.hovered = false;
         });
     });
 }
@@ -103,26 +106,58 @@ function bindCountryUnhover() {
 // clicking on the country (map or list)
 function bindCountryClick() {
     $("svg#map-svg path:not(.greyed-out), ul#country-pick li").on("click", function () {
-        // take all countries off active if single country mode is on
+        // there can only be one country selected
         if (no_countries_mode == 1) {
-            $("svg#map-svg path:not(.greyed-out)").each(function () {
-                $(this).removeClass("active").attr("fill", country_colours.normal);
-                $("li#li-" + this.dataset.country).removeClass("active");
+            // deselect active country
+            $("svg#map-svg path:not(.greyed-out)[data-active=true]").each(function () {
+                $(this).attr("fill", country_colours.normal)[0].dataset.active = false;
+                $("li#li-" + this.dataset.country)[0].dataset.active = false;
             });
+
+            // puts flag and name in the first country slot
+            $("#country-title-1 > .flag-div > img").attr("src", "images/flags/flag-" + this.dataset.country + ".png").show();
+            $("#country-title-1 > .country-name").html(this.dataset.country.replaceAll("-"," "));
+            $("#country-title-1 > .close-country")
+                .attr("title", "Remove " + this.dataset.country.replaceAll("-"," ") + " from selected countries")
+                .attr("onclick", "closeCountry(\"" + this.dataset.country + "\")")
+                .show();
+
+            // stores country name as first one selected
+            active_countries[0] = this.dataset.country;
         }
 
-        // permanently 
+        // there can be two countries selected
+        else if (no_countries_mode == 2) {
+
+        }
+
+        // permanently select clicked country
         $("svg#map-svg path[data-country=" + this.dataset.country + "], ul#country-pick li#li-" + this.dataset.country).each(function () {
-            $(this).attr("fill", country_colours.hover).addClass("active");
-            $("li#li-" + this.dataset.country).addClass("active").removeClass("hovered");
+            $(this).attr("fill", country_colours.active)[0].dataset.active = true;
+            $("li#li-" + this.dataset.country)[0].dataset.active = true;
+            $("li#li-" + this.dataset.country)[0].dataset.hovered = false;
         });
 
-        $("#country-title-1 > .flag-div > img").attr("src", "images/flags/flag-" + this.dataset.country + ".png").show();
-        $("#country-title-1 > .country-name").html(this.dataset.country.replaceAll("-"," "));
-        $("#country-title-1 > .close-country")
-            .attr("title", "Remove " + this.dataset.country.replaceAll("-"," ") + " from selected countries")
-            .show();
     });
+}
+
+function closeCountry(country) {
+    // deselect country on map and list
+    $("svg#map-svg path[data-country=" + country + "], ul#country-pick li#li-" + country).each(function () {
+        $(this).attr("fill", country_colours.normal)[0].dataset.active = false;
+        $("li#li-" + country)[0].dataset.active = false;
+    });
+
+    // remove from selected country bar
+    var index = active_countries.indexOf(country) + 1;
+    active_countries[index - 1] = "";
+    $("#country-title-" + index).replaceWith(
+        "<div class=\"country-title\" id=\"country-title-" + index + "\">\
+        <div class=\"flag-div\"><img src=\"\" height=\"36\" style=\"display:none;\"></div>\
+        <span class=\"country-name\"></span>\
+        <span class=\"close-country\" title=\"\" style=\"display:none;\">❌</span>\
+        </div>"
+    );
 }
 
 // what to do when he page loads
