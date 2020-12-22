3fecedaff5d3bc97d1eca7dfbb2368f746ca5ba3
João Francisco Nogueira Silva Afonso <36608051+YamiJapani@users.noreply.github.com>
2020-12-22 12:04:50 +0000

    gauge

--- script.js
+++ script.js
@@ -82,6 +82,7 @@ $(window).on("load", function () {
     resizeScatterplot();
     createPlayersBarChart();
     resizePlayersBarChart();
+    createGauge();
 
     bindCountryHover();
     bindCountryUnhover();
@@ -1805,6 +1806,215 @@ function bindPlayerClick() {
     });
 }
 
+/* ================================================================ */
+/*                        (8) NT PROFICIENCY                        */
+/* ================================================================ */
+
+function createGauge() {
+    var container = "#power-gauge"
+    var configuration = {
+		size: 380,
+		clipWidth: 380,
+		clipHeight: 380,
+		ringWidth: 80,
+		minValue: -0.2,
+		maxValue: 0.2,
+		transitionMs: 4000,
+	};
+	var that = {};
+	var config = {
+		size						: 200,
+		clipWidth					: 200,
+		clipHeight					: 110,
+		ringInset					: 20,
+		ringWidth					: 20,
+		
+		pointerWidth				: 10,
+		pointerTailLength			: 5,
+		pointerHeadLengthPercent	: 0.9,
+		
+		minValue					: -0.2,
+		maxValue					: 0.2,
+		
+		minAngle					: -135,
+		maxAngle					: 135,
+		
+		transitionMs				: 750,
+		
+		majorTicks					: 5,
+		labelFormat					: d3.format(',g'),
+		labelInset					: 10,
+		
+		arcColorFn					: d3.interpolateHsl(d3.rgb('#ff0000'), d3.rgb('#00ff00'))
+	};
+	var range = undefined;
+	var r = undefined;
+	var pointerHeadLength = undefined;
+	var value = 0;
+	
+	var svg = undefined;
+	var arc = undefined;
+	var scale = undefined;
+	var ticks = undefined;
+	var tickData = undefined;
+	var pointer = undefined;
+
+	//var donut = d3.layout.pie();
+	
+	function deg2rad(deg) {
+		return deg * Math.PI / 180;
+	}
+	
+	function newAngle(d) {
+		var ratio = scale(d);
+		var newAngle = config.minAngle + (ratio * range);
+		return newAngle;
+	}
+	
+	function configure(configuration) {
+		var prop = undefined;
+		for ( prop in configuration ) {
+			config[prop] = configuration[prop];
+		}
+		
+		range = config.maxAngle - config.minAngle;
+		r = config.size / 2;
+		pointerHeadLength = Math.round(r * config.pointerHeadLengthPercent);
+
+		// a linear scale that maps domain values to a percent from 0..1
+		scale = d3.scaleLinear()
+			.range([0,1])
+			.domain([config.minValue, config.maxValue]);
+			
+		ticks = [[-0.16,"Extreme Underscorer"], [-0.08,"Underscorer"], [0,"Neutral"], [0.08,"Top Scorer"], [0.16,"National Pride"]]
+        tickData = d3.range(config.majorTicks).map(function() {return 1/config.majorTicks;});
+		
+		arc = d3.arc()
+			.innerRadius(r - config.ringWidth - config.ringInset)
+			.outerRadius(r - config.ringInset)
+			.startAngle(function(d, i) {
+				var ratio = d * i;
+				return deg2rad(config.minAngle + (ratio * range));
+			})
+			.endAngle(function(d, i) {
+				var ratio = d * (i+1);
+				return deg2rad(config.minAngle + (ratio * range));
+			});
+	}
+	that.configure = configure;
+	
+	function centerTranslation() {
+		return 'translate('+r +','+ r +')';
+	}
+	
+	function isRendered() {
+		return (svg !== undefined);
+	}
+	that.isRendered = isRendered;
+	
+	function render(newValue) {
+		svg = d3.select(container)
+			.append('svg:svg')
+				.attr('class', 'gauge')
+				.attr('width', config.clipWidth)
+				.attr('height', config.clipHeight);
+		
+		var centerTx = centerTranslation();
+		
+		var arcs = svg.append('g')
+				.attr('class', 'arc')
+				.attr('transform', centerTx);
+		
+		arcs.selectAll('path')
+				.data(tickData)
+			.enter().append('path')
+				.attr('fill', function(d, i) {
+					return config.arcColorFn(d * i);
+				})
+                .attr('d', arc);
+                
+		var lg = svg.append('g')
+				.attr('class', 'label')
+				.attr('transform', centerTx);
+		lg.selectAll('text')
+				.data(ticks)
+			.enter().append('text')
+				.attr('transform', function(d) {
+					var ratio = scale(d[0]);
+					var newAngle = config.minAngle + (ratio * range);
+					return 'rotate(' +newAngle +') translate(0,' +(config.labelInset - r + 40) +')';
+                }).call(o => o.append("tspan")
+                    .attr("dy", "1em")
+                    .attr("x", "0")
+                    .text(d => d[1].split(" ")[0]))
+                    .call(o => o.append("tspan")
+                    .attr("dy", "1em")
+                    .attr("x", "0")
+                    .text(d => d[1].split(" ")[1]))
+
+		var lineData = [ [config.pointerWidth / 2, 0], 
+						[0, -pointerHeadLength],
+						[-(config.pointerWidth / 2), 0],
+						[0, config.pointerTailLength],
+                        [config.pointerWidth / 2, 0] ];
+
+        var scaleR = d3.scaleLinear()
+                        .domain([-0.2, 0.2])
+                        .range([deg2rad(-135) - Math.PI/2, deg2rad(135) - Math.PI/2]);
+
+        var center = r - 55;
+        
+        svg.append("circle")
+            .attr("cx", polarToCartesian(r,r,center,center,scaleR(0.2))["x"])
+            .attr("cy", polarToCartesian(r,r,center,center,scaleR(0.2))["y"])
+            .attr("r", 10)
+            .attr("fill", "white")
+            .attr("style", "opacity: 0.85")
+                        
+		/*  var pointerLine = d3.line().interpolate('monotone');
+		var pg = svg.append('g').data([lineData])
+				.attr('class', 'pointer')
+				.attr('transform', centerTx);
+				
+		pointer = pg.append('path')
+			.attr('d', pointerLine/*function(d) { return pointerLine(d) +'Z';}/ )
+			.attr('transform', 'rotate(' +config.minAngle +')');*/
+			
+		update(newValue === undefined ? 0 : newValue);
+	}
+	that.render = render;
+	
+	function update(newValue, newConfiguration) {
+		if ( newConfiguration  !== undefined) {
+			configure(newConfiguration);
+		}
+		var ratio = scale(newValue);
+		var newAngle = config.minAngle + (ratio * range);
+		/*pointer.transition()
+			.duration(config.transitionMs)
+			.ease('elastic')
+			.attr('transform', 'rotate(' +newAngle +')');*/
+	}
+	that.update = update;
+
+	configure(configuration);
+	that.render();
+	return that;
+};
+/*function onDocumentReady() {
+	powerGauge.render();
+	
+	function updateReadings() {
+		// just pump in random data here...
+		powerGauge.update(Math.random() * 10);
+	}
+	
+	// every few seconds update reading values
+	updateReadings();
+	setInterval(function() {
+		updateReadings();
+	}, 5 * 1000);
+}*/
 /* ================================================================ */
 /*                         GENERAL FUNCTIONS                        */
 /* ================================================================ */
